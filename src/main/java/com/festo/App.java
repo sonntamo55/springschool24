8d7f711fffda8bea97f669bc213beff13ac25efa
glidingfreddy <frederik.widmaier@gmail.com>
2024-06-06 10:48:29 +0200

    pom with variable

--- src/main/java/com/festo/App.java
+++ src/main/java/com/festo/App.java
@@ -2,6 +2,7 @@ package com.festo;
 
 import com.festo.communication.BackendComm;
 import com.festo.communication.Direction;
+import com.festo.communication.Movement;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -18,30 +19,9 @@ import javafx.stage.Stage;
 public class App extends Application {
     static String robot = "gilbert"; //Name of our Robot
 
+    Movement warehouse = new Movement();
+    warehouse.setnameRobot(robot);
 
-    @Override
-    public void start(Stage stage) {
-        var lb = new Label("Result: ");
-        Button bt = new Button("Move ricky to east");
-        bt.setOnAction((e) -> {
-            int statusCode = BackendComm.moveRobot( robot, 2);
-            if (statusCode == 200) {
-                lb.setText("Result: SUCCESS");
-            } else {
-                lb.setText("Result: FAILURE");
-            }
-        });
-        VBox vb = new VBox();
-        vb.setPadding(new Insets(10, 10, 10, 10));
-        vb.setSpacing(10);
-        vb.getChildren().addAll(bt, lb);
-        var scene = new Scene(vb, 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    public static void main(String[] args) {
-        launch();
-    }
 
 }
--- src/main/java/com/festo/communication/Movement.java
+++ src/main/java/com/festo/communication/Movement.java
@@ -1,5 +1,20 @@
 package com.festo.communication;
 
 public class Movement {
+
+private String robot;
+
+    public void setnameRobot(String robot) {
+        this.robot = robot;
+    }
+
+
+
+
+
+
+
+
+
     
 }

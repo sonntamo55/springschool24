65a77d75e1a51ad955b4ebb2d5f07474a1f950b2
sime200992 <simon.groess@freenet.de>
2024-06-06 12:36:36 +0200

    Buttons to move EAST, NORTH,SOUTH and WEST

--- src/main/java/com/festo/App.java
+++ src/main/java/com/festo/App.java
@@ -5,9 +5,11 @@ import com.festo.communication.Direction;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,10 +21,86 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        //moveRob(stage);
+        moveRobWeb(stage);
+        
+    }
+
+    private void moveRobWeb(Stage stage)
+    {
+        Pane pane = new Pane();
         var lb = new Label("Result: ");
-        Button bt = new Button("Move ricky to east");
+        //Label lb = new Label("Result: ");
+        lb.setPrefHeight(50);
+        lb.setAlignment(Pos.CENTER);
+        //lb.setVisible(false);
+        Button north = new Button("Move North");
+        north.setPrefHeight(20);
+        north.setLayoutX(200);
+        north.setLayoutY(300);
+        north.setAlignment(Pos.CENTER);
+        north.setOnAction((e) -> {
+            int statusCode = BackendComm.moveRobot("jason", Direction.NORTH);
+            if (statusCode == 200) {
+                lb.setText("Result: SUCCESS Move North");
+            } else {
+                lb.setText("Result: FAILURE Move North");
+            }
+        });
+        Button south = new Button("Move South");
+        south.setPrefHeight(20);
+        south.setLayoutX(10);
+        south.setLayoutY(300);
+        south.setAlignment(Pos.CENTER);
+        south.setOnAction((e) -> {
+            int statusCode = BackendComm.moveRobot("jason", Direction.SOUTH);
+            if (statusCode == 200) {
+                lb.setText("Result: SUCCESS Move South");
+            } else {
+                lb.setText("Result: FAILURE Move South");
+            }
+        });
+        Button east = new Button("Move East");
+        east.setPrefHeight(20);
+        east.setLayoutX(200);
+        east.setLayoutY(350);
+        east.setAlignment(Pos.CENTER);
+        east.setOnAction((e) -> {
+            int statusCode = BackendComm.moveRobot("jason", Direction.EAST);
+            if (statusCode == 200) {
+                lb.setText("Result: SUCCESS Move East");
+            } else {
+                lb.setText("Result: FAILURE Move East");
+            }
+        });
+        Button west = new Button("Move West");
+        west.setPrefHeight(20);
+        west.setLayoutX(10);
+        west.setLayoutY(350);        
+        west.setAlignment(Pos.CENTER);
+        west.setOnAction((e) -> {
+            int statusCode = BackendComm.moveRobot("jason", Direction.WEST);
+            if (statusCode == 200) {
+                lb.setText("Result: SUCCESS Move West");
+            } else {
+                lb.setText("Result: FAILURE Move West");
+            }
+        });
+
+        pane.getChildren().addAll(north, west, east, south, lb);
+
+        Scene scene = new Scene(pane, 600, 400);
+        stage.setScene(scene);
+        stage.setTitle("First Game");
+        stage.show();
+    }
+
+    private void moveRob(Stage stage)
+    {
+        var lb = new Label("Result: ");
+        Button bt = new Button("Move jason to east");
         bt.setOnAction((e) -> {
-            int statusCode = BackendComm.moveRobot("ricky", Direction.EAST);
+            int statusCode = BackendComm.moveRobot("jason", Direction.NORTH);
             if (statusCode == 200) {
                 lb.setText("Result: SUCCESS");
             } else {

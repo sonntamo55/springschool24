b9abf4d8efe9690c9acc9dd5c51d8629fa2aca0b
Pingiumlaranja <pingium.laranja@protonmail.com>
2024-06-06 10:52:30 +0200

    classGui

--- src/main/java/com/festo/App.java
+++ src/main/java/com/festo/App.java
@@ -2,6 +2,7 @@ package com.festo;
 
 import com.festo.communication.BackendComm;
 import com.festo.communication.Direction;
+import com.festo.communication.Gui;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -16,30 +17,11 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-    static String robot = "gilbert"; //Name of our Robot
-
 
     @Override
     public void start(Stage stage) {
-        var lb = new Label("Result: ");
-        Button bt = new Button("Move ricky to east");
-        bt.setOnAction((e) -> {
-            int statusCode = BackendComm.moveRobot( robot, Direction.EAST);
-            if (statusCode == 200) {
-                lb.setText("Result: SUCCESS");
-            } else {
-                lb.setText("Result: FAILURE");
-            }
-        });
-        VBox vb = new VBox();
-        vb.setPadding(new Insets(10, 10, 10, 10));
-        vb.setSpacing(10);
-        vb.getChildren().addAll(bt, lb);
-        var scene = new Scene(vb, 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        new Gui().start(stage);
     }
-
     public static void main(String[] args) {
         launch();
     }
